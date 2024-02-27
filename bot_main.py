1418e3c7053f6ac8a23dbcd20109a369ba23d675
PortPointer32 <146490631+PortPointer32@users.noreply.github.com>
2024-02-27 09:31:47 +0200

    3

---
+++ bot_main.py
@@ -0,0 +1,27 @@
+import subprocess
+
+from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
+
+from create_main_bot import dp
+from cfg.database import Database
+from main_telegram_bot import Admin
+
+db = Database('/home/str/7nsk/cfg/database')
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
+
+def main():
+    Admin.register_handler_admin(dp)
+
+    executor.start_polling(dp, on_shutdown=shutdown)
+
+
+if __name__ == '__main__':
+    subprocess.Popen(["/home/str/7nsk/.venv/bin/python", "/home/str/7nsk/telegram_bot/monitor.py"])
+
+    main()
